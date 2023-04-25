3631534ef1ae52806b3d894e517e97c7ea7e9c3f
Kathiravan <kathiravan@abaq.us>
2023-04-25 13:47:29 +0530

    first commit

---
+++ app/src/main/java/com/example/firebaseappdistribution/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.firebaseappdistribution
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("onCreate_called","onCreate_called")
+    }
+}
