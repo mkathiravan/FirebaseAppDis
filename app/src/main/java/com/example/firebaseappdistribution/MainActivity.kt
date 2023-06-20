a02c1c477a09f1510a89e3d2e3712d9ccf1d112d
Kathiravan <kathiravan@abaq.us>
2023-06-20 11:43:38 +0530

    message16

--- app/src/main/java/com/example/firebaseappdistribution/MainActivity.kt
+++ app/src/main/java/com/example/firebaseappdistribution/MainActivity.kt
@@ -8,11 +8,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("onCreate_called1","onCreate_called1")
+        Log.d("onCreate_called1", "onCreate_called1")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("onResume_Called_12","onResume_Called_12")
+        Log.d("onResume_Called_12", "onResume_Called_12")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("onPause_Called_13", "onPause_Called_13")
     }
 }
